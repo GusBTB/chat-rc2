195e123160e1da7ef30822ac64c3ec82fa0d869f
Vinícius Araújo Messias <vinicius.messias4321@gmail.com>
2026-05-15 16:51:22 -0300

    mudancas e testes
    adicionei hash na senha e fiz rodar com testes

--- chat/src/main/java/br/edu/chat/App.java
+++ chat/src/main/java/br/edu/chat/App.java
@@ -1,6 +1,9 @@
 package br.edu.chat;
 
 import br.edu.chat.database.DatabaseInitializer;
+import br.edu.chat.testmanual.DatabaseUserTest;
+import br.edu.chat.testmanual.ModelTest;
+import br.edu.chat.testmanual.RepositoryTestMain;
 
 /**
  * Hello world!
@@ -10,7 +13,10 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Iniciando Chat TCP...");
 
-        DatabaseInitializer.initialize();
+        //DatabaseInitializer.initialize();
+        RepositoryTestMain.main(args);
+        ModelTest.main(args);
+        DatabaseUserTest.main(args);
 
         System.out.println("Aplicacao finalizada.");
     }
